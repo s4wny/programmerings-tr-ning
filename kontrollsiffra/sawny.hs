9f55993d6525a83fa23a051c064d99d12858d7b9
Sony? <sony@4morefun.net>
2014-05-09 20:58:09 +0200

    Improved kontrollsiffra/haskell

--- kontrollsiffra/sawny.hs
+++ kontrollsiffra/sawny.hs
@@ -1,17 +1,16 @@
 import System.IO (getLine)
-
+ 
 toInt :: [String] -> [Int]
 toInt = map read
-
+ 
 -- Get every nth element in a list
 every :: Int -> [Int] -> [Int]
 every _ []     = []
 every n (x:xs) = x : every n (drop (n-1) xs)
-
+ 
 main = do
     stdin <- getLine
     let numbers = toInt $ words stdin
-    let total   = sum [foldl (\acc x -> acc + x*3) 0 (every 2 numbers),
-                       foldl (\acc x -> acc + x*7) 0 (every 2 (tail numbers))]
-
+    let total   = sum (every 2 numbers) * 3 + sum (every 2 $ tail numbers) * 7
+ 
     print $ total `mod` 10
