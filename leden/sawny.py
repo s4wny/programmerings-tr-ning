f81ddce2c4146fd7c88b97f30911db51088c9bc2
Sony? <sony@4morefun.net>
2014-05-10 12:46:14 +0200

    My notes about leden

--- leden/sawny.py
+++ leden/sawny.py
@@ -6,6 +6,6 @@ wayPoints = map(int, data);
 time      = 0;
  
 for i, wayPoint in enumerate(wayPoints[0:-1]):
-    time  = time + abs(-5 - -1*(wayPoint-wayPoints[i+1]))*2 + 60;    
+    time  = time + abs(5 - (wayPoint-wayPoints[i+1]))*2 + 60;
  
 print time;
