f0d278d4c91246ab02e6d2889aa146b7f9414b05
Alexander Bladh <alexander.bladh@hotmail.com>
2015-02-02 17:02:17 +0100

    Update xobust.cpp

--- vandrarhem/xobust.cpp
+++ vandrarhem/xobust.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 int main()
 {
-    int M, N, a, b, price = 0,i; map<int, int> d;
+    int M, N, a, b, p = 0,i; map<int, int> d;
     cin >> N >> M;
     for(i = 0; i<M; i++)
     {   cin >> a >>b;
         d[a] += b;
     }
     while(N--)
-    {   price += d.begin()->first;
+    {   p += d.begin()->first;
         if(--d.begin()->second == 0)
             d.erase(d.begin());
     }
-    cout << price; return 0;
+    cout << p;
 }
